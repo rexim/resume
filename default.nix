11e1f2c26c701dc12e7bed94ce6024748237b737
rexim <reximkut@gmail.com>
2017-06-17 16:20:08 +0700

    Deprecate textLiveFull NixOS package

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 with import <nixpkgs> {}; {
   resumeEnv = stdenv.mkDerivation {
     name = "resume";
-    buildInputs = [ stdenv python27Full python27Packages.virtualenv texLiveFull ];
+    buildInputs = [ stdenv python27Full python27Packages.virtualenv texlive.combined.scheme-full ];
     shellHook = ''
       if [ ! -d venv ]; then
         virtualenv --python=python2.7 venv
